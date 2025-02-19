19fc2285da909f4e97dbf07e753b4a2c2650a100
Sowmya Marriyapillai Ravisandiran <163436053+SowmyaMRavisandiran@users.noreply.github.com>
2025-02-19 14:31:53 +0100

    Add files via upload

--- plotting_results.py
+++ plotting_results.py
@@ -29,10 +29,10 @@ opt_df = montecarlo_result[['F2', 'F4', 'F6', 'F7','F9', 'F12','F14']]
 
 #opt_df = montecarlo_result.iloc[:,:25]
 
-densities = [gaussian_kde(opt_df[column]) for column in opt_df.columns]  # Calculate KDE for each column
+in_densities = [gaussian_kde(opt_df[column]) for column in opt_df.columns]  # Calculate KDE for each column
 
 joint_prob_density = [
-    np.prod([densities[i](row[i]) for i in range(len(row))]) for idx, row in opt_df.iterrows()
+    np.prod([in_densities[i](row[i]) for i in range(len(row))]) for idx, row in opt_df.iterrows()
 ]  # Calculate joint probability density for each row
 
 max_index = np.argmax(joint_prob_density)  # Find index of the max probability density
@@ -41,23 +41,9 @@ optimal_combination = opt_df.iloc[max_index]  # Select the corresponding row
 final_result = montecarlo_result.iloc[max_index]
 
 
-
-#%% Getting the 90% confidence intervals
-
-out_range = pd.DataFrame()
-for idx, col in enumerate(opt_df.columns):
-    
-    x_values = np.linspace(min(opt_df[col]), max(opt_df[col]), 1000)
-    kde_values = densities[idx](x_values)
-    percentile_5 = np.percentile(opt_df[col], 5)
-    percentile_95 = np.percentile(opt_df[col], 95)
-    out_range[col]=[percentile_5, percentile_95]    
-
-
 #%% Plot 1: input variables
 
 in_df = montecarlo_result[['F2', 'F4', 'F6', 'F7','F9', 'F12','F14']]  # Select input variable from the results
-
 
 #in_df_column_names = {'F2':'Collection by Itinerant Waste Buyers', 'F6':'Collection by wastepickers from bins',
 #                 'F9':'Collection of Recyclable Plastic by BOVs', 'F14':'Aggregation in scrap shops',
@@ -114,16 +100,10 @@ for row in range(2):
 
         i += 1
         
-#plt.savefig('plots/input_variables.png', dpi = 300)
+plt.savefig('plots/input_variables.png', dpi = 300)
 plt.show()
 
-
-#%%% Plot histograms alone
-montecarlo_result.hist(bins=30, figsize=(20, 20))
-plt.suptitle("Histograms of Variables")
-plt.show()
-
-#%%% Plot output rates
+#%%% Plot 2: output rates
 
 out_rates_df = montecarlo_result[['Mismanagement Rate', 'Management Rate', 'Recovery Rate', 'Recycling Rate','RP Recovery Rate', 'NRP Recovery Rate','IWS Recovery Rate','PET Recovery Rate']]
 out_rates_densities = [gaussian_kde(out_rates_df[column]) for column in out_rates_df.columns]  # Calculate KDE for each column
@@ -165,13 +145,7 @@ plt.show()
 #plt.savefig('plots/output_rates_pdf.png', dpi = 300)
 
 
-
-#%% Plot output vs input 
-
-
-fig, axes = plt.subplots(2, 4, figsize=(20, 10))  
-fig.tight_layout(pad=4.0)  # Adjust layout 
-i=0  
+#%% Plot 3: output vs input 
 
 in_df_columns = {'F2':'F2: Collection by IWBs', 'F4':'F4: Collection by FS in corporation bins ', 
                        'F6':'F6: Collection by wastepickers from bins', 'F7':'F6: Collection by wastepickers from landfills',
@@ -179,6 +153,10 @@ in_df_columns = {'F2':'F2: Collection by IWBs', 'F4':'F4: Collection by FS in co
                        'F14':'F14: Aggregation in scrap shops'}
 for output in out_rates_df.columns:
     print(output)
+    
+    i=0  
+    fig, axes = plt.subplots(2, 4, figsize=(20, 10))  
+    fig.tight_layout(pad=4.0)  # Adjust layout 
     for row in range(2):
         for col in range(4):
             if i < len(in_df.columns):  # Ensure we only plot 7 subplots
@@ -208,14 +186,42 @@ for output in out_rates_df.columns:
     plt.show()
     #plt.savefig('plots/pet_recovery_rate_vs_inputs.png', dpi = 300)
 
-#%%
+#%% Getting the output with max likelihood values
 
-out_flow_df = montecarlo_result[['F1', 'F5', 'F10', 'F18','F19', 'F20','F21','F22','F23']]
-out_flow_df.rename(columns = {'F1':'F1: Plastic waste generated from households', 'F5':'F5: Uncollected waste',
-                              'F10':'F10: Recyclable Plastic scrap sold by waste pickers to small scrap shops', 
-                              'F18':'F18: Recycled plastic sent for manufacturing',
-                              'F19':'F19: Non-recyclable plastic sent for pyrolysis', 'F20':'F20: Non-recyclable plastic sent to cement plants',
-                              'F21':'F21: Plastic in unsegregated waste dumped in landfills','F22':'F22: Uncollected plastic waste that ends up in litter',
-                              'F23':'F23: Uncollected plastic waste that is openly burned'}, inplace = True)
-out_flow_densities = [gaussian_kde(out_flow_df[column]) for column in out_flow_df.columns]  # Calculate KDE for each column
 
+#%%% Getting the 90% confidence intervals 
+
+densities = [gaussian_kde(montecarlo_result[column]) for column in montecarlo_result.columns]  # Calculate KDE for each column
+ranges = pd.DataFrame()
+for idx, col in enumerate(montecarlo_result.columns):
+    
+    x_values = np.linspace(min(montecarlo_result[col]), max(montecarlo_result[col]), 1000)
+    kde_values = densities[idx](x_values)
+    percentile_5 = np.percentile(montecarlo_result[col], 5)
+    percentile_95 = np.percentile(montecarlo_result[col], 95)
+    ranges[col]=[percentile_5, percentile_95]    
+
+#%%% Combining results and generating output
+ranges.rename(index={0: "5th Percentile", 1: "95th Percentile"},inplace=True)
+ranges=ranges.transpose()
+ranges.reset_index(names=['Variable'],inplace = True)
+
+final_result=pd.DataFrame(final_result).reset_index(names=['Variable'])
+
+ranges = pd.merge(ranges,final_result,how='left', on='Variable')
+
+#Update columns NAme
+col_name=ranges.columns[3]
+ranges.rename(columns={col_name:'Max Likelihood solution'},inplace= True)
+
+ranges['Unit']='tons per month'
+for var in ranges['Variable'].unique():
+    if var.endswith('Rate'):
+        ranges.loc[ranges['Variable']==var,'Unit']='%'
+    elif var.endswith(')'):
+        ranges.loc[ranges['Variable']==var,'Unit']='kg per person per day'
+
+columns = ranges.columns
+ranges = ranges[[columns[0],columns[1],columns[3],columns[2],columns[4]]]
+
+ranges.to_csv('Results.csv',index=False)
